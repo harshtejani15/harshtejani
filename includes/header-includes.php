352452eb82db3d1970afc47962b23ef2e7c64fd8
harshtejani15 <139966746+harshtejani15@users.noreply.github.com>
2023-10-13 13:10:05 +0530

    done

--- includes/header-includes.php
+++ includes/header-includes.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8" />
 <meta name="HandheldFriendly" content="true">
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
-<link type="image/x-icon" rel="shortcut icon" href="images/favicon.png" />
+<link type="image/x-icon" rel="shortcut icon" href="images/favicon.svg" />
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/all.min.css">
 <link rel="stylesheet" type="text/css" href ="css/animatedheadline.css">
